85cdf37aa845a7cf8c61c4863bf62b410680e41f
Ricardo Benitez <33403205+RRbenitez77@users.noreply.github.com>
2021-08-25 17:58:42 -0400

    mapping

--- logic.js
+++ logic.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 // Create a map object
+=======
+
+
+>>>>>>> 43734d6 (maping)
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson"
 
@@ -7,6 +12,10 @@ var query2 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
+<<<<<<< HEAD
+=======
+  console.log(data.features);
+>>>>>>> 43734d6 (maping)
   createFeatures(data.features);
 });
 
@@ -31,7 +40,11 @@ function createFeatures(earthquakeData) {
       var r = 255;
       var g = Math.floor(255-80*feature.properties.mag);
       var b = Math.floor(255-80*feature.properties.mag);
+<<<<<<< HEAD
       color= "rgb("+r+" ,"+g+","+ b+")"
+=======
+      color= getColor(feature.properties.mag)
+>>>>>>> 43734d6 (maping)
       
       var geojsonMarkerOptions = {
         radius: 4*feature.properties.mag,
@@ -46,6 +59,11 @@ function createFeatures(earthquakeData) {
   });
 
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 43734d6 (maping)
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
   
@@ -55,8 +73,12 @@ function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+<<<<<<< HEAD
     "access_token=pk.eyJ1IjoicnJiZW5pdGV6MTk3NyIsImEiOiJja3F2aXl1azQwZXloMm9tdGM2bHFrbW1iIn0.N34bIy5l_gsbQmZML586eA." +
     "T6YbdDixkOBWH_k9GbS8JQ");
+=======
+    "access_token=pk.eyJ1IjoicGF5bW9uMTIzIiwiYSI6ImNrc3J6NmRtMzByajMydm81bHM1eWQ4NGoifQ.BxJIPVjwfFunxFuvOlq-EQ");
+>>>>>>> 43734d6 (maping)
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -78,6 +100,7 @@ function createMap(earthquakes) {
   });
 
 
+<<<<<<< HEAD
   function getColor(d) {
       return d < 1 ? 'rgb(255,255,255)' :
             d < 2  ? 'rgb(255,225,225)' :
@@ -90,6 +113,9 @@ function createMap(earthquakes) {
             d < 9  ? 'rgb(255,15,15)' :
                         'rgb(255,0,0)';
   }
+=======
+
+>>>>>>> 43734d6 (maping)
 
   // Create a legend to display information about our map
   var legend = L.control({position: 'bottomright'});
@@ -115,3 +141,19 @@ function createMap(earthquakes) {
   legend.addTo(myMap);
 
 }
+<<<<<<< HEAD
+=======
+
+function getColor(d) {
+  return d < 1 ? 'rgb(255,255,255)' :
+        d < 2  ? 'rgb(255,225,225)' :
+        d < 3  ? 'rgb(255,195,195)' :
+        d < 4  ? 'rgb(255,165,165)' :
+        d < 5  ? 'rgb(255,135,135)' :
+        d < 6  ? 'rgb(255,105,105)' :
+        d < 7  ? 'rgb(255,75,75)' :
+        d < 8  ? 'rgb(255,45,45)' :
+        d < 9  ? 'rgb(255,15,15)' :
+                    'rgb(255,0,0)';
+}
+>>>>>>> 43734d6 (maping)
